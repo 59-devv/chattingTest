b7e67f95d19ce0fc40f2d51d8a054ac2f0a7a6f1
59 <5.9.devv@gmail.com>
2021-12-25 01:55:18 +0900

    레디스 성공

---
+++ src/main/java/com/example/chattingtest/webSocket/ChatController.java
@@ -0,0 +1,40 @@
+package com.example.chattingtest.webSocket;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class ChatController {
+    private final SimpMessageSendingOperations messagingTemplate;
+    private final ChatService chatService;
+    private final ChatRoomRepository chatRoomRepository;
+    private final RedisPublisher redisPublisher;
+
+    @MessageMapping("/chat/message")
+    public void message(ChatMessage message) {
+        if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
+            chatRoomRepository.enterChatRoom(message.getRoomId());
+            message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+        }
+        // Websocket에 발행된 메시지를 redis로 발행한다(publish)
+        redisPublisher.publish(chatRoomRepository.getTopic(message.getRoomId()), message);
+    }
+
+    @PostMapping
+    public ChatRoom createRoom(@RequestParam String name) {
+        return chatService.createRoom(name);
+    }
+
+    @GetMapping
+    public List<ChatRoom> findAllRoom() {
+        return chatService.findAllRoom();
+    }
+
+
+}
